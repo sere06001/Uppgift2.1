dbdad7e8d848159dbb3d57e8c7da41f184de4218
sere06001 <sere06001@utb.vaxjo.se>
2023-09-20 10:14:43 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift2_1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Din favorit bok är " + bok + ".");
+            Console.ReadKey();
+        }
+    }
+}
